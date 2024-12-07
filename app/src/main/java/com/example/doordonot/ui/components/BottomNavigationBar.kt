468562bb45f7ab382967cd6bbd6a38beb50234a6
tesabel <woochan0408@naver.com>
2024-12-07 19:10:11 +0900

    delete addHabit in BottomIcon

--- app/src/main/java/com/example/doordonot/ui/components/BottomNavigationBar.kt
+++ app/src/main/java/com/example/doordonot/ui/components/BottomNavigationBar.kt
@@ -2,23 +2,16 @@ package com.example.doordonot.ui.components
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CalendarToday
-import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
-
 @Composable
 fun BottomNavigationBar(navController: NavController) {
     NavigationBar {
-        NavigationBarItem(
-            icon = { Icon(Icons.Default.Edit, contentDescription = "습관 추가") },
-            label = { Text("습관 추가") },
-            selected = false,
-            onClick = { navController.navigate("make_habit") }
-        )
+        // "습관 추가" 버튼 삭제함
         NavigationBarItem(
             icon = { Icon(Icons.Default.CalendarToday, contentDescription = "캘린더") },
             label = { Text("캘린더") },
@@ -31,7 +24,7 @@ fun BottomNavigationBar(navController: NavController) {
             selected = false,
             onClick = { navController.navigate("habit_management") }
         )
-        // 설정 탭 추가
+        // 설정 탭
         NavigationBarItem(
             icon = { Icon(Icons.Default.Settings, contentDescription = "설정") },
             label = { Text("설정") },
--- app/src/main/java/com/example/doordonot/ui/components/TopBar.kt
+++ app/src/main/java/com/example/doordonot/ui/components/TopBar.kt
@@ -1,12 +1,22 @@
 package com.example.doordonot.ui.components
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopBar(title: String) {
+fun TopBar(title: String, navController: NavController? = null) {
     TopAppBar(
-        title = { Text(text = title) }
+        title = { Text(text = title) },
+        navigationIcon = {
+            navController?.let {
+                IconButton(onClick = { it.popBackStack() }) {
+                    Icon(Icons.Default.ArrowBack, contentDescription = "뒤로가기")
+                }
+            }
+        }
     )
 }
